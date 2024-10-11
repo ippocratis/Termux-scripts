09275b6a8d4ba03dc7f021d25996e85cc004867d
ᓰᕵᕵᓍ <52239579+ippocratis@users.noreply.github.com>
2024-10-11 21:51:06 +0300

    Create historian.sh
    Take a bugreport

---
+++ historian.sh
@@ -0,0 +1,7 @@
+Battery historian
+
+https://developer.android.com/topic/performance/power/setup-battery-historian
+
+Open a root terminal and type `bugreportz`
+device will vibrate and it will output a zip file under/data/user_de/0/com.android.shell/files/bugreports
+Upload and analize it at https://bathist.dev
